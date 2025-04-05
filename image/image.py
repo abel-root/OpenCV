c6dc76a14a8e9c17cbbdd6c3d291b0b87cc17577
Abel <k.kouakouabel96@gmail.com>
2025-04-05 18:37:38 +0200

    save image

--- image/image.py
+++ image/image.py
@@ -2,14 +2,17 @@ import cv2 as cv
 import numpy as np
 
 #load image 
-img = cv.imread("Photo24.png") 
+img = cv.imread("Photo24.png",0) 
 
 #show image 
 cv.namedWindow("mage_1")
 cv.imshow("image_3",img)
 
 #update windows show 
-cv.waitKey(0) 
+key=cv.waitKey(0) 
+
+if key==ord("s"):
+    cv.imwrite("new_image.png",img)
 
 #destroy all windows is openned 
 cv.destroyAllWindows()
